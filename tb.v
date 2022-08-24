9597bb79c57ccb9631479d72a695471381d1c22a
Micuks <sdkjwql@gmail.com>
2022-08-24 22:02:13 +0800

    add testbench

---
+++ tb.v
@@ -0,0 +1,83 @@
+module cpu_tb;
+
+  reg [3:0] sw;
+  reg [7:4] ir;
+  reg [3:0] s;
+  reg
+      clr,
+      t3,
+      swa,
+      swb,
+      swc,
+      w1,
+      w2,
+      w3,
+      c,
+      z,
+      drw,
+      pcinc,
+      lpc,
+      lar,
+      pcadd,
+      arinc,
+      selctl,
+      memw,
+      stop,
+      lir,
+      ldz,
+      ldc,
+      cin,
+      m,
+      abus,
+      sbus,
+      mbus,
+      short,
+      long,
+      sel0,
+      sel1,
+      sel2,
+      sel3;
+
+  cpu cpu_inst (
+      .clr(clr),
+      .t3(t3),
+      .swa(swa),
+      .swb(swb),
+      .swc(swc),
+      .w1(w1),
+      .w2(w2),
+      .w3(w3),
+      .c(c),
+      .z(z),
+      .drw(drw),
+      .pcinc(pcinc),
+      .lpc(lpc),
+      .lar(lar),
+      .pcadd(pcadd),
+      .arinc(arinc),
+      .selctl(selctl),
+      .memw(memw),
+      .stop(stop),
+      .lir(lir),
+      .ldz(ldz),
+      .ldc(ldc),
+      .cin(cin),
+      .m(m),
+      .abus(abus),
+      .sbus(sbus),
+      .mbus(mbus),
+      .short(short),
+      .long(long),
+      .sel0(sel0),
+      .sel1(sel1),
+      .sel2(sel2),
+      .sel3(sel3)
+  );
+
+  initial begin
+    sw = 3'b000;
+    ir = 4'b0001;
+
+  end
+
+endmodule
