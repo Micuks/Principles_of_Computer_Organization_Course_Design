eceaf6d151a8a6c314b0cd82dcef7e8bb069e34b
Micuks <sdkjwql@gmail.com>
2022-08-24 22:01:59 +0800

    rewrite bool_func

--- pick_ir.sv
+++ pick_ir.sv
@@ -2,6 +2,7 @@
 
 function bool_func;
 
+<<<<<<< HEAD
   input [7:0] ir;
   input [7:0] sign;
 
@@ -122,6 +123,57 @@ task pick_ir_st0_1(input [7:0] ir, output lir, pcinc, output [3:0] s, output cin
       ir, axor
   ) || bool_func(
       ir, dec
+  )));
+=======
+  input [7:4] ir;
+  input [7:4] sign;
+
+  //   integer i;
+  //   reg result = 1;
+  begin
+    // for (i = 7; i > 3; i = i - 1) begin
+    //   result = result && (sign[i]) ? ir[i] : !ir[i];
+    //   $display("sign[%0d] = %0b", i, sign[i]);
+    // end
+    // bool_func = result;
+    bool_func = (ir == sign);
+  end
+endfunction
+
+task pick_ir_st0_1(input [7:4] ir, output lir, pcinc, output [3:0] s, output cin, abus, drw, ldz,
+                   ldc, m, lar, long, input c, output pcadd, input z, output lpc, stop, mbus, memw,
+                   input w1, w2, w3, output short);
+  lir <= w1;
+  pcinc <= w1;
+  short <= 0;
+
+  s[3] <= ((w2 && (bool_func(  //0001,0010,0101,0110(w3),1001,1011
+      ir, 0001
+  ) || bool_func(
+      ir, 0010
+  ) || bool_func(
+      ir, 0101
+  ) || bool_func(
+      ir, 0110
+  ) || bool_func(
+      ir, 1001
+  ) || bool_func(
+      ir, 1011
+  ))) || w3 && bool_func(
+      ir, 0110
+  ));
+>>>>>>> d5257de (rewrite bool_func)
+
+  s[2] <= (w2 && (bool_func(
+      ir, 0010
+  ) || bool_func(
+      ir, 0110
+  ) || bool_func(
+      ir, 1001
+  ) || bool_func(
+      ir, 1010
+  ) || bool_func(
+      ir, 1011
   )));
 
   s[1] <= ((w2 && (bool_func(
