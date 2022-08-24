d5257deed625cbdb39a7813cd0e228ea3ec0aba6
Micuks <sdkjwql@gmail.com>
2022-08-24 22:01:59 +0800

    rewrite bool_func

--- pick_ir.sv
+++ pick_ir.sv
@@ -1,14 +1,17 @@
 function bool_func;
+
   input [7:4] ir;
   input [7:4] sign;
-  integer i;
-  reg result = 1;
+
+  //   integer i;
+  //   reg result = 1;
   begin
-    for (i = 7; i > 3; i = i - 1) begin
-      result = result && (sign[i]) ? ir[i] : !ir[i];
-      $display("sign[%0d] = %0b", i, sign[i]);
-    end
-    bool_func = result;
+    // for (i = 7; i > 3; i = i - 1) begin
+    //   result = result && (sign[i]) ? ir[i] : !ir[i];
+    //   $display("sign[%0d] = %0b", i, sign[i]);
+    // end
+    // bool_func = result;
+    bool_func = (ir == sign);
   end
 endfunction
 
@@ -18,26 +21,34 @@ task pick_ir_st0_1(input [7:4] ir, output lir, pcinc, output [3:0] s, output cin
   lir <= w1;
   pcinc <= w1;
   short <= 0;
-  s[3] <= ((w2 && (
-    (!ir[7] && !ir[6] && ir[4]) || //0001,0010,0101,0110(w3),1001,1011
-    (!ir[7] && ir[6] && !ir[5] && ir[4]) ||
-                (!ir[7] && ir[6] && ir[5] && !ir[4]) ||
-    (ir[7] && !ir[6] && ir[4]))) ||
-    (w3 && (
-    !ir[7] && ir[6] && ir[5] && !ir[4])
-    )
-    );
 
-  s[2] <= (w2 && ((!ir[7] && !ir[6] && ir[5] && !ir[4]) ||  // 0010
-  (!ir[7] && ir[6] && ir[5] && !ir[4]) ||  //0110
-  (ir[7] && !ir[6] && !ir[5] && ir[4]) ||  //1001
-  (bool_func(
-      ir, 1010
-  )) ||  //1010
-  (bool_func(
+  s[3] <= ((w2 && (bool_func(  //0001,0010,0101,0110(w3),1001,1011
+      ir, 0001
+  ) || bool_func(
+      ir, 0010
+  ) || bool_func(
+      ir, 0101
+  ) || bool_func(
+      ir, 0110
+  ) || bool_func(
+      ir, 1001
+  ) || bool_func(
       ir, 1011
-  ))  //1011
+  ))) || w3 && bool_func(
+      ir, 0110
   ));
+
+  s[2] <= (w2 && (bool_func(
+      ir, 0010
+  ) || bool_func(
+      ir, 0110
+  ) || bool_func(
+      ir, 1001
+  ) || bool_func(
+      ir, 1010
+  ) || bool_func(
+      ir, 1011
+  )));
 
   s[1] <= ((w2 && (bool_func(
       ir, 0010
